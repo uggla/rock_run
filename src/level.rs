bd9c021ef56e5a60cdfaefda0719b4a670304d34
Uggla <uggla@free.fr>
2024-05-02 00:27:57 +0200

    feat: Toggle menu background visibility

--- src/level.rs
+++ src/level.rs
@@ -13,6 +13,9 @@ pub struct CurrentLevel(Level);
 #[derive(Component, Eq, PartialEq)]
 pub struct Level(u8);
 
+#[derive(Component)]
+struct Menu;
+
 pub struct LevelPlugin;
 
 impl Plugin for LevelPlugin {
@@ -20,11 +23,17 @@ impl Plugin for LevelPlugin {
         app.add_systems(Startup, setup_background)
             .add_systems(
                 OnEnter(AppState::GameCreate),
-                toggle_level_background_visibility,
+                (
+                    toggle_level_background_visibility,
+                    toggle_menu_background_visibility,
+                ),
             )
             .add_systems(
                 OnEnter(AppState::StartMenu),
-                toggle_level_background_visibility,
+                (
+                    toggle_level_background_visibility,
+                    toggle_menu_background_visibility,
+                ),
             )
             .insert_resource(CurrentLevel(Level(1)));
     }
@@ -39,7 +48,7 @@ fn setup_background(mut commands: Commands, asset_server: Res<AssetServer>) {
             transform: Transform::from_xyz(-(WINDOW_WIDTH / 2.0 - 720.0 / 2.0), 0.0, 0.0),
             ..default()
         },
-        Level(0),
+        Menu,
     ));
 
     let map_handle: Handle<helpers::tiled::TiledMap> = asset_server.load("level01.tmx");
@@ -69,5 +78,14 @@ fn toggle_level_background_visibility(
                 }
             }
         }
+    }
+}
+
+fn toggle_menu_background_visibility(mut menu: Query<&mut Visibility, With<Menu>>) {
+    let mut visibility = menu.single_mut();
+    if *visibility == Visibility::Visible {
+        *visibility = Visibility::Hidden;
+    } else {
+        *visibility = Visibility::Visible;
     }
 }
