bfc6009fc1638c4c05cd1991e5877101a0d44b09
Uggla <uggla@free.fr>
2024-05-24 17:09:54 +0200

    feat: Display iyes_perf_ui pressing F12, and add build and run aliases
    - This feature is only available for debug mode and native build as it
      is not compatible with wasm.
    
    - Add aliases, this allow to manage bevy/dynamic_linking properly:
      - bd  --> build debug.  (enable dynamic_linking + perf_ui)
      - br  --> build release.
      - bwd --> build wasm debug.
      - bwr --> build wasm release.
      - rd  --> run debug.
      - rr  --> run release.
      - rwd --> run wasm debug.
      - rwr --> run wasm release.
    
    - Bump a couple of dependencies (enum_iterator, thiserror).

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,8 @@ pub const WINDOW_WIDTH: f32 = 1280.0;
 pub const WINDOW_HEIGHT: f32 = 720.0;
 
 fn main() {
-    App::new()
-        .init_state::<AppState>()
+    let mut app = App::new();
+    app.init_state::<AppState>()
         .add_plugins((
             DefaultPlugins
                 .set(WindowPlugin {
@@ -69,8 +69,33 @@ fn main() {
             ),
         )
         .add_event::<StoryMessages>()
-        .add_event::<NoMoreStoryMessages>()
-        .run();
+        .add_event::<NoMoreStoryMessages>();
+
+    #[cfg(all(debug_assertions, not(target_arch = "wasm32")))]
+    app.add_systems(
+        Update,
+        toggle_perf_ui.before(iyes_perf_ui::PerfUiSet::Setup),
+    );
+
+    app.run()
+}
+
+#[allow(dead_code)]
+fn toggle_perf_ui(
+    mut commands: Commands,
+    q_root: Query<Entity, With<iyes_perf_ui::PerfUiRoot>>,
+    kbd: Res<ButtonInput<KeyCode>>,
+) {
+    if kbd.just_pressed(KeyCode::F12) {
+        if let Ok(e) = q_root.get_single() {
+            // despawn the existing Perf UI
+            commands.entity(e).despawn_recursive();
+        } else {
+            // create a simple Perf UI with default settings
+            // and all entries provided by the crate:
+            commands.spawn(iyes_perf_ui::PerfUiCompleteBundle::default());
+        }
+    }
 }
 
 // TODO: remove as this is for debugging purpose
